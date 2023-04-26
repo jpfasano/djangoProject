1b9afc93e1d6d7b2166ed00153274b4c1e570823
JP Fasano <jpfasano@gmail.com>
2023-04-26 17:10:16 -0600

    Completed Corey Schafer Part 11

--- routes/models.py
+++ routes/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Route(models.Model):
@@ -13,3 +14,6 @@ class Route(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('route-detail', kwargs={'pk':self.pk})
--- routes/views.py
+++ routes/views.py
@@ -1,16 +1,96 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+
+from django.urls import reverse
+from django.shortcuts import get_object_or_404
 from .models import Route
+from django.contrib.auth.models import User
+
 
 def routes(request):
     context = {
         'routes': Route.objects.all(),
         'title': 'Routes'
     }
-    return render(request,'routes/routes.html', context)
+    return render(request, 'routes/routes.html', context)
+
+
+class RoutesListView(ListView):
+    model = Route
+    template_name = 'routes/routes.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'routes'
+    ordering = ['-date_posted']  # Newest to oldest
+    paginate_by = 5
+
+
+class UserRoutesListView(ListView):
+    model = Route
+    template_name = 'routes/user_routes.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'routes'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Route.objects.filter(author=user).order_by('-date_posted')
+
+class RoutesDetailView(DetailView):
+    model = Route
+
+
+class RoutesCreateView(LoginRequiredMixin, CreateView):
+    model = Route
+    fields = ['title', 'description', 'distance', 'start_location']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class RoutesUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Route
+    fields = ['title', 'description', 'distance', 'start_location']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    # Check to make sure user is allowed to update the route.
+    # Only the route's author is allowed to update.
+    def test_func(self):
+        route = self.get_object()
+        if self.request.user == route.author:
+            return True
+        else:
+            return False
+
+
+class RoutesDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Route
+
+    # success_url = '/'
+    # Check to make sure user is allowed to delete the route.
+    # Only the route's author is allowed to delete.
+    def test_func(self):
+        route = self.get_object()
+        if self.request.user == route.author:
+            return True
+        else:
+            return False
+
+    def get_success_url(self):
+        return reverse('routes')
 
 
 def about(request):
-    return render(request,'routes/about.html', {'title': 'About Routes'})
+    return render(request, 'routes/about.html', {'title': 'About Routes'})
+
 
 def home(request):
-    return render(request,'routes/home.html', {'title': 'ADKEzRiders Home Page'})
+    return render(request, 'routes/home.html', {'title': 'ADKEzRiders Home Page'})
