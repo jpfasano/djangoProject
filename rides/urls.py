aa95d8de60c1cf070b75bdccf3ecbeb9ed11c815
JP Fasano <jpfasano@gmail.com>
2023-05-12 07:08:38 -0600

    Create Ride Report changed to be Update Ride Report

--- rides/urls.py
+++ rides/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path('rides/user/<str:username>/', UserRidesListView.as_view(), name='user-rides'),
     path('rides/<int:pk>/', RidesDetailView.as_view(), name='ride-detail'),
     path('rides/new/', RidesCreateView.as_view(), name='ride-create'),
-    path('rides/<int:pk>/new-report/', RidesReportCreateView.as_view(), name='ride-report-create'),
+    path('rides/<int:pk>/update-report/', RidesReportCreateView.as_view(), name='ride-report-create'),
     path('rides/<int:pk>/update/', RidesUpdateView.as_view(), name='ride-update'),
     path('rides/<int:pk>/delete/', RidesDeleteView.as_view(), name='ride-delete'),
 ]
